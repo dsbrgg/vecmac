fab9d0e9f7d7624a471bef3429d25a809396491b
dsbrgg <dsbrgg@gmail.com>
2020-07-31 22:18:57 +0100

    more expressions

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@
 // this is completely up to the user of the macro as all of them as valid
 // and have the same meaning in this context
 
+// Why use it?
+// Declarative macros are good to express repetition
+// eg. implement a trait for a type that the logic is the same everytime
+// 44:16 at the reference video
+// #[derive] logic is only for PROC MACROS, no declarative macros
+
+// macro_rules also does not have TRAIT BOUNDS
+
 #[macro_export]
 macro_rules! avec {
     // empty_vec ------------
@@ -31,13 +39,31 @@ macro_rules! avec {
     // double --------------
     // $() syntax will expand to a pattern
     // `,+` means: one or more inputs, separated by comma
-    ($($element:expr),+) => {{
+    // `$(,)?` means that it will accept a trailling comma, where `?` means zero or one
+    // declarative macros accept only `?`, `+` and `*` for patterns, so its not
+    // an actual regex engine
+    ($($element:expr),+ $(,)?) => {{
         let mut vs = Vec::new();
         // `*` token will execute the expression as many times as
         // the variable $element appear in the pattern created
         $(vs.push($element);)*
         vs
-    }}
+    }};
+
+    // clone ---------------------
+    // its always good to evaluate an expression just once
+    // eg. let count = $count;
+    ($element:expr; $count:expr) => {{
+        let count = $count;
+        let mut vs = Vec::with_capacity(count);
+        // this is a good way to allocate to a vec when the size is known
+        // .extend will make rust know that the vector will have elements added to it
+        // and preallocate the correct size for it instead of reallocating the pointer on each
+        // iteration
+        // std::iter::repeat will yield clones of the element given as long as you .take them
+        vs.extend(std::iter::repeat($element).take(count));
+        vs
+    }};
 }
 
 #[test]
@@ -61,4 +87,24 @@ fn double() {
     assert_eq!(x.len(), 2);
     assert_eq!(x[0], 42);
     assert_eq!(x[1], 43);
+}
+
+#[test]
+fn trail() {
+    let x: Vec<&'static str> = avec!["ola", "ola", "ola",];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 3);
+    assert_eq!(x[0], "ola");
+    assert_eq!(x[1], "ola");
+    assert_eq!(x[2], "ola");
+}
+
+#[test]
+fn clone() {
+    let mut y = Some(42);
+    let x: Vec<u32> = avec![y.take().unwrap(); 2];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 42);
 }
